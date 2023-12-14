002db5d1d4e84f2e6dc12d32b3704481a3e1831c
krisjanisp <krisjanispetrucena@gmail.com>
2023-12-14 11:23:57 +0200

    add postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [],
 }
-
